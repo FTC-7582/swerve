4780efccfcbc69d76b2aebf4231676849d01707a
Jeff Micono <usfirst@micono.com>
2015-12-29 00:20:44 -0700

    UNTESTED -- make changes to support the grabber (both the rack and the rotator)
    ====================================
    CONFIG UPDATES
    
    NEW: rack_controller (motor controller)
    NEW: +- grabber_rack (dc Motor)
    
    CHANGE: wrist becomes grabberRotator on channel 1
    
    ====================================
    COMMANDING
    
    Use GamePad 2
    +left_stick_y means grabberRack up
    -left_stick_y means grabberRack down
    
    Y means grabberRotator set to 0.5
    X means grabberRotator left  + 5 / 180.0
    B means grabberRotator right - 5 / 180.0

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FourWheelDriveOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FourWheelDriveOp.java
@@ -60,27 +60,29 @@ public class FourWheelDriveOp extends OpMode {
     // amount to change the claw servo position by
     double clawDelta = 0.01;
 
-    // position of the wrist servo
-    double wristPosition;
-
-    // amount to change the wrist servo position by
-    double wristDelta = 0.01;
-
-    // command for the continuous servo
-    double continuousPosition;
-    static double continuousStop = 0.5;
-    double continuousDelta = 0.05;
-
     DcMotorController wheelControllerFront;
     DcMotorController wheelControllerRear;
+    DcMotorController grabberRackController;
+
     DcMotor motorRightFront;
     DcMotor motorLeftFront;
     DcMotor motorRightRear;
     DcMotor motorLeftRear;
+    DcMotor grabberRack;
 
     Servo claw;
-    Servo wrist;
+
+    Servo grabberRotator;
+    static double grabberRotatorMiddle = 0.5;
+    static double grabberRotatorStep = 0.5 / 180.0; // 0.5 degrees
+    double grabberRotatorPosition = grabberRotatorMiddle;
+
     Servo continuous;
+    double continuousPosition;
+    static double continuousLeft = 0.0;
+    static double continuousMiddle = 0.5;
+    static double continuousStep = 0.05;
+    static double continuousRight = 1.0;
 
     TelemetryDashboardAndLog dashboard;
 
@@ -102,6 +104,14 @@ public class FourWheelDriveOp extends OpMode {
         motorLeftFront = hardwareMap.dcMotor.get("motor_lt_front");
         motorLeftRear = hardwareMap.dcMotor.get("motor_lt_rear");
 
+        try {
+            grabberRack = hardwareMap.dcMotor.get("grabber_rack");
+        }
+        catch (Exception E)
+        {
+            grabberRack = null;
+        }
+
         List<Servo> activeServos = new ArrayList<Servo>();
 
         /// code to make the servos optional
@@ -116,12 +126,12 @@ public class FourWheelDriveOp extends OpMode {
         }
 
         try {
-            wrist = hardwareMap.servo.get("wrist"); // channel 1
-            activeServos.add(wrist);
+            grabberRotator = hardwareMap.servo.get("grabberRotator"); // channel 1
+            activeServos.add(grabberRotator);
         }
         catch (Exception E)
         {
-            wrist = null;
+            grabberRotator = null;
         }
 
         try {
@@ -136,11 +146,20 @@ public class FourWheelDriveOp extends OpMode {
         wheelControllerFront = hardwareMap.dcMotorController.get("wheels_front");
         wheelControllerRear = hardwareMap.dcMotorController.get("wheels_rear");
 
+        try {
+            grabberRackController = hardwareMap.dcMotorController.get("rack_controller");
+        }
+        catch (Exception E)
+        {
+            grabberRackController = null;
+        }
+
         /// after the call to createEasyMotorController, the motor controllers
         /// work normally -- no looping mod 17 or waiting for the mode to switch
         ///
         createEasyMotorController(this, motorRightFront, motorLeftFront);
         createEasyMotorController(this, motorRightRear, motorLeftRear);
+        if (grabberRackController != null) { createEasyMotorController(this, grabberRack, null); }
 
         /// swerve robootics interface makes the servos more synchronous
         ///
@@ -149,19 +168,35 @@ public class FourWheelDriveOp extends OpMode {
         /// set up the dashboard with callbacks that are activated when needed
         /// supplants the telemetry object
         ///
-        dashboard.addLine(dashboard.item("continuous", new IFunc<Object>()
+        dashboard.addLine(dashboard.item("continuous ", new IFunc<Object>()
             {
-                @Override public Double value() {return continuousPosition;}
+                @Override public Double value() {return continuous.getPosition();}
             }));
 
-        dashboard.addLine(dashboard.item("front L/R motor", new IFunc<Object>()
+        dashboard.addLine(dashboard.item("front L/R motor ", new IFunc<Object>()
         {
-            @Override public String value() {return Double.toString(motorLeftFront.getPower()) + "/"+ Double.toString(motorRightFront.getPower());}
+            @Override public String value() {return String.format("%2f", motorLeftFront.getPower()) + "/"+ String.format("%2f", motorRightFront.getPower());}
         }));
 
-        dashboard.addLine(dashboard.item("rear L/R motor", new IFunc<Object>()
+        dashboard.addLine(dashboard.item("rear L/R motor ", new IFunc<Object>()
         {
-            @Override public String value() {return Double.toString(motorLeftRear.getPower()) + "/" + Double.toString(motorRightRear.getPower());}
+            @Override public String value() {return String.format("%2f", motorLeftRear.getPower()) + "/" + String.format("%2f", motorRightRear.getPower());}
+        }));
+
+        /// there is a chance during testing that either the grabberRack or the grabberRotator will be missing
+        /// the callback (e.g. lambda) function checks if either of these is null and simply returns a blank.
+        ///
+        dashboard.addLine(dashboard.item("rack motor/servo ", new IFunc<Object>() {
+            @Override
+            public String value() {
+                String grabberRackAsString = "";
+                String grabberRotatorAsString = "";
+
+                if (grabberRack != null) { grabberRackAsString = String.format("%.2f", grabberRack.getPower()); }
+                if (grabberRotator != null) { grabberRotatorAsString = String.format("%.2f", grabberRotator.getPosition()); }
+
+                return grabberRackAsString + "/" + grabberRotatorAsString;
+            }
         }));
 
         DbgLog.msg("Four Wheel Drive Initialized");
@@ -174,8 +209,6 @@ public class FourWheelDriveOp extends OpMode {
     @Override
     public void init_loop() {
 
-//        devMode = DcMotorController.DeviceMode.WRITE_ONLY;
-
         /// only the motor on the left side runs in reverse
         ///
         motorLeftFront.setDirection(DcMotor.Direction.REVERSE);
@@ -187,10 +220,11 @@ public class FourWheelDriveOp extends OpMode {
         motorRightFront.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
         motorLeftRear.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
         motorRightRear.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        if (grabberRack != null) { grabberRack.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS); }
 
-        wristPosition = 0.6;
         clawPosition = 0.5;
-        continuousPosition = continuousStop;
+        continuousPosition = continuousMiddle;
+        grabberRotatorPosition = grabberRotatorMiddle;
     }
 
     /*
@@ -206,126 +240,138 @@ public class FourWheelDriveOp extends OpMode {
      * Gamepad 1 controls the motors via the left stick, and it controls the wrist/claw via the a,b,
      * x, y buttons
      */
-            if (gamepad1.dpad_left) {
-                motorLeftFront.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-                motorRightFront.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-                motorLeftRear.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-                motorRightRear.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
-            }
-            if (gamepad1.dpad_right) {
-                motorLeftFront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-                motorRightFront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-                motorLeftRear.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-                motorRightRear.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-            }
-
-            // throttle:  left_stick_y ranges from -1 to 1, where -1 is full up,  and 1 is full down
-            // direction: left_stick_x ranges from -1 to 1, where -1 is full left and 1 is full right
-            float right = -gamepad1.right_stick_y;
-            float left = -gamepad1.left_stick_y;
-
-            // clip the right/left values so that the values never exceed +/- 1
-            right = Range.clip(right, -1, 1);
-            left = Range.clip(left, -1, 1);
-
-            // write the values to the motors
-            motorRightFront.setPower(right);
-            motorLeftFront.setPower(left);
-            motorRightRear.setPower(right);
-            motorLeftRear.setPower(left);
-
-            //Continuous servo
-            if (continuous != null) {
-                if (gamepad1.x) {
-                    continuous.setPosition(0.0f);
-                } else if (gamepad1.y) {
-                    continuous.setPosition(0.5f);
-                } else if (gamepad1.b) {
-                    continuous.setPosition(1.0f);
-                }
-            }
-
-            // update the position of the wrist
-//      if (gamepad1.a) {
-//        wristPosition -= wristDelta;
-//      }
-//
-//      if (gamepad1.y) {
-//        wristPosition += wristDelta;
-//      }
-//
-//      // update the position of the claw
-//      if (gamepad1.x) {
-//        continuousPosition -= continuousDelta;
-//        clawPosition -= clawDelta;
-//      }
-//
-//      if (gamepad1.b) {
-//        continuousPosition += continuousDelta;
-//        clawPosition += clawDelta;
-//      }
-
-            // clip the position values so that they never exceed 0..1
-            wristPosition = Range.clip(wristPosition, 0, 1);
-            clawPosition = Range.clip(clawPosition, 0, 1);
-            continuousPosition = Range.clip(continuousPosition, 0, 1);
-
-            // if we get outside the interval
-            if (Math.abs(continuousPosition - continuousStop) < continuousDelta / 2.0)
-            {
-                continuousPosition = continuousStop;
-            }
-
-            // write position values to the wrist and claw servo
-            if (wrist != null) { wrist.setPosition(wristPosition); }
-            if (claw != null) { claw.setPosition(clawPosition); }
-
-    /*
-     * Gamepad 2
-     *
-     * Gamepad controls the motors via the right trigger as a throttle, left trigger as reverse, and
-     * the left stick for direction. This type of control is sometimes referred to as race car mode.
-     */
-
-            // we only want to process gamepad2 if someone is using one of it's analog inputs. If you always
-            // want to process gamepad2, remove this check
-      /*
-      if (gamepad2.atRest() == false) {
-
-        // throttle is taken directly from the right trigger, the right trigger ranges in values from
-        // 0 to 1
-        throttle = gamepad2.right_trigger;
-
-        // if the left trigger is pressed, go in reverse
-        if (gamepad2.left_trigger != 0.0) {
-          throttle = -gamepad2.left_trigger;
+        if (gamepad1.dpad_left) {
+            motorLeftFront.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+            motorRightFront.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+            motorLeftRear.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+            motorRightRear.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        }
+        if (gamepad1.dpad_right) {
+            motorLeftFront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+            motorRightFront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+            motorLeftRear.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+            motorRightRear.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         }
 
-        // assign throttle to the left and right motors
-        right = throttle;
-        left = throttle;
+        if (grabberRack != null) { grabberRack.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS); }
 
-        // now we need to apply steering (direction). The left stick ranges from -1 to 1. If it is
-        // negative we want to slow down the left motor. If it is positive we want to slow down the
-        // right motor.
-        if (gamepad2.left_stick_x < 0) {
-          // negative value, stick is pulled to the left
-          left = left * (1 + gamepad2.left_stick_x);
-        }
-        if (gamepad2.left_stick_x > 0) {
-          // positive value, stick is pulled to the right
-          right = right * (1 - gamepad2.left_stick_x);
-        }
+        // throttle:  left_stick_y ranges from -1 to 1, where -1 is full up,  and 1 is full down
+        // direction: left_stick_x ranges from -1 to 1, where -1 is full left and 1 is full right
+        float right = -gamepad1.right_stick_y;
+        float left = -gamepad1.left_stick_y;
 
-        // write the values to the motor. This will over write any values placed while processing gamepad1
+        // clip the right/left values so that the values never exceed +/- 1
+        right = Range.clip(right, -1, 1);
+        left = Range.clip(left, -1, 1);
+
+        // write the values to the motors
         motorRightFront.setPower(right);
         motorLeftFront.setPower(left);
         motorRightRear.setPower(right);
         motorLeftRear.setPower(left);
-      } */
 
-        /// telemetry will get updated regularly and sent back without saturating the link or processor
+        /// Continuous servo
+        /// X means set continuousPosition to 0.0 -- go left
+        /// Y means add a delta
+        /// B means set continuousPosition to 1.0 -- go right
+        /// A means subtract a delta
         ///
-        dashboard.update();
+        if (gamepad1.x) {
+            continuousPosition = 0.0f;
+        } else if (gamepad1.y) {
+            continuousPosition += continuousStep;
+        } else if (gamepad1.b) {
+            continuousPosition = 1.0f;
+        } else if (gamepad1.a) {
+            continuousPosition -= continuousStep;
+        }
+
+        /// if we are close enough to the middle, then call it the middle
+        ///
+        if (Math.abs(continuousPosition - continuousMiddle) < continuousStep / 2.0)
+        {
+            continuousPosition = continuousMiddle;
+        }
+
+        /// using gamepad2
+        /// left_stick_y means grabberRack up ranges from -1 to 1, where -1 is full up, and 1 is full down
+        ///
+        float grabberRackAmount = -Range.clip(gamepad2.left_stick_y, -1.0f, 1.0f);
+        if (grabberRack != null) { grabberRack.setPower(grabberRackAmount); }
+
+        /// grabberRotatorPosition using gamepad2
+        /// Y means go to middle
+        /// X means decrease
+        /// B means increase
+        ///
+        if (gamepad2.y) {
+            grabberRotatorPosition = grabberRotatorMiddle;
+        } else if (gamepad2.x) {
+            grabberRotatorPosition -= grabberRotatorStep;
+        } else if (gamepad2.b) {
+            grabberRotatorPosition += grabberRotatorStep;
+        }
+
+        /// if we are close enough to the middle, then call it the middle
+        ///
+        if (Math.abs(grabberRotatorPosition - grabberRotatorMiddle) < grabberRotatorStep / 2.0)
+        {
+            grabberRotatorPosition = grabberRotatorMiddle;
+        }
+
+        // clip the position values so that they never exceed 0..1
+        grabberRotatorPosition = Range.clip(grabberRotatorPosition, 0, 1);
+        clawPosition = Range.clip(clawPosition, 0, 1);
+        continuousPosition = Range.clip(continuousPosition, 0, 1);
+
+        // write position values to the servos
+        if (grabberRotator != null) { grabberRotator.setPosition(grabberRotatorPosition); }
+        if (claw != null) { claw.setPosition(clawPosition); }
+        if (continuous != null) { continuous.setPosition(continuousPosition); }
+
+//    /*
+//     * Gamepad 2
+//     *
+//     * Gamepad controls the motors via the right trigger as a throttle, left trigger as reverse, and
+//     * the left stick for direction. This type of control is sometimes referred to as race car mode.
+//     */
+//
+//            // we only want to process gamepad2 if someone is using one of it's analog inputs. If you always
+//            // want to process gamepad2, remove this check
+//      /*
+//      if (gamepad2.atRest() == false) {
+//
+//        // throttle is taken directly from the right trigger, the right trigger ranges in values from
+//        // 0 to 1
+//        throttle = gamepad2.right_trigger;
+//
+//        // if the left trigger is pressed, go in reverse
+//        if (gamepad2.left_trigger != 0.0) {
+//          throttle = -gamepad2.left_trigger;
+//        }
+//
+//        // assign throttle to the left and right motors
+//        right = throttle;
+//        left = throttle;
+//
+//        // now we need to apply steering (direction). The left stick ranges from -1 to 1. If it is
+//        // negative we want to slow down the left motor. If it is positive we want to slow down the
+//        // right motor.
+//        if (gamepad2.left_stick_x < 0) {
+//          // negative value, stick is pulled to the left
+//          left = left * (1 + gamepad2.left_stick_x);
+//        }
+//        if (gamepad2.left_stick_x > 0) {
+//          // positive value, stick is pulled to the right
+//          right = right * (1 - gamepad2.left_stick_x);
+//        }
+//
+//        // write the values to the motor. This will over write any values placed while processing gamepad1
+//        motorRightFront.setPower(right);
+//        motorLeftFront.setPower(left);
+//        motorRightRear.setPower(right);
+//        motorLeftRear.setPower(left);
+//      } */
+        dashboard.update(); // telemetry will get updated regularly and sent back without saturating the link or processor
     }
 }
