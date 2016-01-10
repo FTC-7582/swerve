0a9a3c7fb10bc3ebf3db73552ddfb999eef295c8
Padwolf <padwolf99@gmail.com>
2016-01-10 15:40:05 -0700

    I corrected the autonomous functions. The left and right turning cause a communication error with the robot, likely from overstressed motors or something to that effect.

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/GriffinOpMode.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/GriffinOpMode.java
@@ -34,18 +34,20 @@ public class GriffinOpMode extends SynchronousOpMode
         this.drive[2] = this.hardwareMap.dcMotor.get("motor_lt_rear");
         this.drive[3] = this.hardwareMap.dcMotor.get("motor_rt_rear");
 
-        createEasyMotorController(this, drive[1], drive[0]);
-        createEasyMotorController(this, drive[3], drive[2]);
+        createEasyMotorController(this, drive[2], drive[0]);
+        createEasyMotorController(this, drive[3], drive[1]);
 
         // Wait for the game to start
         waitForStart();
 
-        forward(1000);
-        backward(1000);
-        forward(0.5f, 2000);
-        backward(0.25f, 4000);
+        forward(0.5f, 1000);
+        backward(0.5f, 1000);
+        left(0.25f, 500);
+        forward(0.5f, 750);
+        right(0.5f, 1000);
 
-        while (!gamepad1.right_bumper) {}
+        //Wait for gamepad 1's right bumper to be pressed
+        while (!gamepad1.right_bumper) {Thread.sleep(10);}
 
         // Go go gadget robot!
         while (opModeIsActive())
@@ -53,9 +55,9 @@ public class GriffinOpMode extends SynchronousOpMode
             if (updateGamepads())
             {
                 drive[0].setPower(Range.clip(gamepad1.left_stick_y, -1, 1));
-                drive[2].setPower(Range.clip(gamepad1.left_stick_y, -1, 1));
-                drive[1].setPower(Range.clip(gamepad1.right_stick_y, -1, 1));
-                drive[3].setPower(Range.clip(gamepad1.right_stick_y, -1, 1));
+                drive[1].setPower(Range.clip(gamepad1.left_stick_y, -1, 1));
+                drive[2].setPower(-Range.clip(gamepad1.right_stick_y, -1, 1));
+                drive[3].setPower(-Range.clip(gamepad1.right_stick_y, -1, 1));
                 // The game pad state has changed. Do something with that!
             }
 
@@ -65,6 +67,55 @@ public class GriffinOpMode extends SynchronousOpMode
     }
 
     public void forward(long time) throws InterruptedException{
+        drive[0].setPower(-1);
+        drive[2].setPower(-1);
+        drive[1].setPower(1);
+        drive[3].setPower(1);
+
+        Thread.sleep(time);
+
+        for (DcMotor mtr : drive){
+            mtr.setPower(0);
+        }
+    }
+    public void forward(float power, long time) throws InterruptedException{
+        drive[0].setPower(-power);
+        drive[2].setPower(-power);
+        drive[1].setPower(power);
+        drive[3].setPower(power);
+
+        Thread.sleep(time);
+
+        for (DcMotor mtr : drive) {
+            mtr.setPower(0);
+        }
+    }
+
+    public void backward(long time) throws InterruptedException{
+        drive[0].setPower(1);
+        drive[2].setPower(1);
+        drive[1].setPower(-1);
+        drive[3].setPower(-1);
+
+        Thread.sleep(time);
+
+        for (DcMotor mtr : drive){
+            mtr.setPower(0);
+        }
+    }
+    public void backward(float power, long time) throws InterruptedException{
+        drive[0].setPower(power);
+        drive[2].setPower(power);
+        drive[1].setPower(-power);
+        drive[3].setPower(-power);
+
+        Thread.sleep(time);
+
+        for (DcMotor mtr : drive){
+            mtr.setPower(0);
+        }
+    }
+    public void left(long time) throws InterruptedException{
         for (DcMotor mtr : drive){
             mtr.setPower(1);
         }
@@ -75,19 +126,18 @@ public class GriffinOpMode extends SynchronousOpMode
             mtr.setPower(0);
         }
     }
-    public void forward(float power, long time) throws InterruptedException{
-        for (DcMotor mtr : drive) {
+    public void left(float power, long time) throws InterruptedException{
+        for (DcMotor mtr : drive){
             mtr.setPower(power);
         }
 
         Thread.sleep(time);
 
-        for (DcMotor mtr : drive) {
+        for (DcMotor mtr : drive){
             mtr.setPower(0);
         }
     }
-
-    public void backward(long time) throws InterruptedException{
+    public void right(long time) throws InterruptedException{
         for (DcMotor mtr : drive){
             mtr.setPower(-1);
         }
@@ -98,7 +148,7 @@ public class GriffinOpMode extends SynchronousOpMode
             mtr.setPower(0);
         }
     }
-    public void backward(float power, long time) throws InterruptedException{
+    public void right(float power, long time) throws InterruptedException{
         for (DcMotor mtr : drive){
             mtr.setPower(-power);
         }
